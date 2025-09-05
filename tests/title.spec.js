d8ac72107f443247e12f2289cb2d1d7a03edd400
RonnPC <201801673@est.umss.edu>
2025-09-04 21:10:48 -0400

    Commit Title.spec.js

---
+++ tests/title.spec.js
@@ -0,0 +1,24 @@
+const { test, expect } = require('@playwright/test');
+
+test('valida el título exacto', async ({ page }) => {
+    //abre la pagina example.com
+    await page.goto('https://example.com/');
+    await sleep (5000);
+
+    // Verificamos el titulo exacto
+    await expect(page).toHaveTitle('Example Domain');
+});
+
+test ('Valida titulo con regex (contiene palabra)', async ({page}) => {
+     //abre la pagina example.com
+    await page.goto('https://example.com/');
+    await sleep (5000);
+
+    // Verificamos palabra
+    await expect(page).toHaveTitle(/Example/);
+
+});
+
+function sleep(time) {
+  return new Promise((resolve) => setTimeout(resolve, time));
+}
